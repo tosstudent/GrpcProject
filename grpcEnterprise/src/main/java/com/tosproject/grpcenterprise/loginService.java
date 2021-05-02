f8814a7f41ac56270847cbf9ce76eb673745e37b
tosstudent <37158579+tosstudent@users.noreply.github.com>
2021-05-02 02:29:32 +0100

    implement login service API

--- grpcEnterprise/src/main/java/com/tosproject/grpcenterprise/loginService.java
+++ grpcEnterprise/src/main/java/com/tosproject/grpcenterprise/loginService.java
@@ -1,10 +1,32 @@
 package com.tosproject.grpcenterprise;
 
-public class loginService {
+import com.tosproject.grpcenterprise.LoginServiceGrpc.LoginServiceImplBase;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import io.grpc.stub.StreamObserver;
 
+public class loginService extends LoginServiceImplBase{
+
+	@Override
+	public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
+		System.out.println("Loign here");
+		String firstname = request.getFirstname();
+		String lastname = request.getLastname();
+		int membershipNumber =request.getMembershipNumber();
+		LoginResponse.Builder response = LoginResponse.newBuilder();
+		
+		if(membershipNumber == (12345)) {
+			//return valid member message
+			response.setMessage(lastname).setMessage("Valid Member");
+		}
+		else {
+			
+			//return invalid member message
+			response.setMessage(lastname).setMessage("invalid Member");
+		}
+		responseObserver.onNext(response.build());
+		responseObserver.onCompleted();
 	}
+
+	
 
 }
