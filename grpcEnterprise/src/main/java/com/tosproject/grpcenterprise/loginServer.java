18adda6f210a1aa8aafc19455f20fce1d7fd6111
tosstudent <37158579+tosstudent@users.noreply.github.com>
2021-05-02 04:39:03 +0100

    Login Server with port

--- grpcEnterprise/src/main/java/com/tosproject/grpcenterprise/loginServer.java
+++ grpcEnterprise/src/main/java/com/tosproject/grpcenterprise/loginServer.java
@@ -1,22 +1,39 @@
 package com.tosproject.grpcenterprise;
 
+import java.io.IOException;
+
 import com.tosproject.grpcenterprise.LoginServiceGrpc.LoginServiceImplBase;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
-public class loginService extends LoginServiceImplBase{
-
+public class loginServer extends LoginServiceImplBase{
+	
+	//server port
+		public static void main(String[] args) throws IOException, InterruptedException {
+		Server server = ServerBuilder.forPort(48051).addService(new loginServer()).build();
+		server.start();
+		System.out.println("Server started at " + server.getPort());
+		server.awaitTermination();
+		}
+	 
+	
+	//service implementation
+	@SuppressWarnings("unused")
 	@Override
 	public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
+		
+		
 		System.out.println("Loign here");
 		String firstname = request.getFirstname();
 		String lastname = request.getLastname();
 		int membershipNumber =request.getMembershipNumber();
 		LoginResponse.Builder response = LoginResponse.newBuilder();
 		
-		if(membershipNumber == (12345)) {
+		if(membershipNumber <= (899887798)) {
 			//return valid member message
-			response.setMessage(lastname).setMessage("Valid Member");
+			response.setMessage(lastname).setMessage("Your membership is valid, welccome to Cornelius Emergency Service ");
 		}
 		else {
 			
@@ -26,7 +43,5 @@ public class loginService extends LoginServiceImplBase{
 		responseObserver.onNext(response.build());
 		responseObserver.onCompleted();
 	}
-
-	
-
 }
+
